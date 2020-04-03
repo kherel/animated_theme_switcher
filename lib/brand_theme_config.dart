d04506adee1b381da20731ef36eb7384159431bc
Peyman <peiman_3009@yahoo.com>
2020-04-03 13:46:45 +0430

    handle theme in a simpler and more readable way

--- lib/brand_theme_config.dart
+++ lib/brand_theme_config.dart
@@ -1,11 +1,27 @@
 import 'package:flutter/material.dart';
 import 'brand_theme_model.dart';
 
-// In this config file, I'm configuration the themes, usualy I have several folder in my projects
-// config, logic, ui. 
+// In this config file, I'm configuration the themes, usualy I have several
+// folder in my projects
+// config, logic, ui.
 
-ThemeData defaultThemeData = ThemeData(
-  // I changed FAB button form, only as example that you can configurate MaterialTheme too..
+// default lightTheme with customized FAB button form, only as example that you
+// can configurate MaterialTheme too..
+ThemeData defaultThemeData = ThemeData.light().copyWith(
+  floatingActionButtonTheme: FloatingActionButtonThemeData(
+    shape: RoundedRectangleBorder(),
+  ),
+);
+
+// darkTheme for app, you can change it as you want
+final darkTheme = ThemeData(
+  primarySwatch: Colors.grey,
+  primaryColor: Colors.black,
+  brightness: Brightness.dark,
+  cardColor: Color(0xFF212121),
+  backgroundColor: Colors.black,
+  accentColor: Colors.white,
+  dividerColor: Colors.black12,
   floatingActionButtonTheme: FloatingActionButtonThemeData(
     shape: RoundedRectangleBorder(),
   ),
@@ -13,18 +29,10 @@ ThemeData defaultThemeData = ThemeData(
 
 BrandThemeModel lightBrandTheme = BrandThemeModel(
   brightness: Brightness.light,
-  color1: Colors.blue,
-  color2: Colors.white,
-  textColor1: Colors.black,
-  textColor2: Colors.white,
   themeData: defaultThemeData,
 );
 
 BrandThemeModel darkBrandTheme = BrandThemeModel(
   brightness: Brightness.dark,
-  color1: Colors.red,
-  color2: Colors.black,
-  textColor1: Colors.blue,
-  textColor2: Colors.yellow,
-  themeData: defaultThemeData,
+  themeData: darkTheme,
 );
