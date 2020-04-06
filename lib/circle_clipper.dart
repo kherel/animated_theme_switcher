673d188b4911a96657d5a013abe495fc87c6c72f
Kherel <kherel@gmail.com>
2020-04-06 12:30:41 +0200

    create library

--- lib/circle_clipper.dart
+++ lib/circle_clipper.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 
-// this clipper used to animate a growing circle, after the theme switch.
-
 class CircleClipper extends CustomClipper<Path> {
   CircleClipper({this.sizeRate, this.offset});
 
@@ -10,8 +8,6 @@ class CircleClipper extends CustomClipper<Path> {
 
   @override
   Path getClip(Size size) {
-    // Because we are using a stack in main.dart
-    // this size is the size of the whole screen;
     var path = Path()
       ..addOval(
         Rect.fromCircle(center: offset, radius: size.height * sizeRate),
---
+++ lib/theme_switcher.dart
@@ -0,0 +1,30 @@
+import 'package:animated_theme_switcher_example/theme_provider.dart';
+import 'package:flutter/material.dart';
+
+typedef ChangeTheme = void Function(ThemeData theme);
+
+class ThemeSwitcher extends StatefulWidget {
+  const ThemeSwitcher({Key key, this.builder}) : super(key: key);
+
+  final Widget Function(BuildContext context, ChangeTheme changeMyTheme)
+      builder;
+
+  @override
+  _ThemeSwitcherState createState() => _ThemeSwitcherState();
+}
+
+class _ThemeSwitcherState extends State<ThemeSwitcher> {
+  final GlobalKey globalKey = GlobalKey();
+
+  @override
+  Widget build(BuildContext context) {
+    void changeTheme(ThemeData theme) {
+      ThemeProvider.instanceOf(context).changeTheme(theme: theme, key: globalKey);
+    }
+
+    return Container(
+      key: globalKey,
+      child: widget.builder(context, changeTheme),
+    );
+  }
+}
