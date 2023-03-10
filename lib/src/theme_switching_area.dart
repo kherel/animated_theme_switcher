f44983f6da560a423e0bcfcfbfb77c6e0da6950f
dan99dm <38222904+dan99dm@users.noreply.github.com>
2023-03-10 18:30:04 +0100

    fix animations inside ThemeSwitchingArea (#63)
    Co-authored-by: Dan <dandmitriev>

--- lib/src/theme_switching_area.dart
+++ lib/src/theme_switching_area.dart
@@ -1,6 +1,7 @@
+import 'package:flutter/material.dart';
+
 import 'clippers/theme_switcher_clipper_bridge.dart';
 import 'theme_provider.dart';
-import 'package:flutter/material.dart';
 
 class ThemeSwitchingArea extends StatelessWidget {
   ThemeSwitchingArea({
@@ -9,8 +10,6 @@ class ThemeSwitchingArea extends StatelessWidget {
   }) : super(key: key);
 
   final Widget child;
-  //one more key to save drawer state
-  final _globalKey = GlobalKey();
 
   @override
   Widget build(BuildContext context) {
@@ -58,7 +57,7 @@ class ThemeSwitchingArea extends StatelessWidget {
 
   Widget _getPage(ThemeData brandTheme) {
     return Theme(
-      key: _globalKey,
+      key: ValueKey('ThemeSwitchingAreaPage'),
       data: brandTheme,
       child: child,
     );
